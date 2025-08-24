819983008b3eab6544185cb6d3f4237bc95d0220
Pratik Bakshi <bakshi.pratikcool@gmail.com>
2025-08-24 14:00:02 +0530

    tab functionality complete and login design complete

---
+++ Readme.md
@@ -0,0 +1,5 @@
+### Cashly App
+
+A app to build finance tracker app for personal finance management.
+
+tech: HTML, CSS and JavaScript.
---
+++ src/main.js
@@ -0,0 +1,29 @@
+import "./style.css";
+
+// DOM Element
+
+//tab
+const TabButtons = document.querySelectorAll(".tab-buttons .tab");
+const TabContainers = document.querySelectorAll(" .content");
+
+//tab functionality
+TabButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const tabid = btn.dataset.tab;
+
+    //removing existing active class from all tabs button
+    TabButtons.forEach((btn) => btn.classList.remove("active"));
+
+    TabContainers.forEach((content) => content.classList.remove("active"));
+
+    TabContainers.forEach((content) => {
+      const targetContent = content.dataset.tab;
+
+      if (targetContent === tabid) {
+        content.classList.add("active");
+      }
+    });
+
+    btn.classList.add("active");
+  });
+});
